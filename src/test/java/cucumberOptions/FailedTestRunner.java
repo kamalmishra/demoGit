d96e3f0ec086a463f4a0a15d11a41b9bccff7ada
kamalmishra <kamalmishra0589@gmail.com>
2025-07-14 15:03:11 +0530

    updated faild test runner

--- src/test/java/cucumberOptions/FailedTestRunner.java
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features= "@target/failed_scenarios.txt", glue ="stepDefinitions" 
+@CucumberOptions(features= "src/test/java/features", glue ="stepDefinitions" 
 , monochrome= true ,plugin= {"html:target/cucmber.html","json:target/cucumber.json",
 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 
